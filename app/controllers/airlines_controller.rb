835fab34a18bca607cafbdc265e44e2fb301c133
Stiven097 <s@delonix.io>
2023-01-29 02:48:12 -0400

    created destroy method in airlines controller

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,6 +1,6 @@
 class AirlinesController < ApplicationController
 
-  before_action :set_airline, only: [:update, :edit, :show]
+  before_action :set_airline, only: [:update, :edit, :show, :destroy]
 
   def new
     @airline = Airline.new
@@ -42,7 +42,22 @@ class AirlinesController < ApplicationController
     @airline = Airline.find(params[:id])
   end
   
+  def destroy
 
+    airlineId = Flight.where(airline_id: @airline)
+
+    if airlineId.exists?
+      redirect_to airports_path, alert: "Cannot delete airport because there is a flight or more related to it. Please change this relation first."
+    else
+      if @airline.destroy
+        redirect_to request.referrer, notice: "The data has been deleted"
+      else
+        redirect_to request.referrer, flash: {error: @airport.errors.full_messages}
+      end
+    end
+
+  end
+  
   def airline_params
     params.require(:airline).permit(:iata_code, :airline)
   end
